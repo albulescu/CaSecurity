da4f6a46b9326d2a34d0ccdf55f4e8315adeb547
Albulescu Cosmin <cosmin@albulescu.ro>
2013-09-08 11:35:48 +0300

    Fix file docblock

--- src/Rule/AbstractRule.php
+++ src/Rule/AbstractRule.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Albulescu Cosmin ( http://www.albulescu.com/ )
-*
-* @link      http://www.albulescu.com/
-* @copyright Copyright (c) 2012 Albulescu Cosmin. (http://www.albulescu.com)
-* @license   http://www.albulescu.ro/new-bsd New BSD License
-* @autor Albulescu Cosmin <cosmin@albulescu.ro>
-*/
+ * Albulescu Cosmin ( http://www.albulescu.ro/ )
+ *
+ * @link      http://www.albulescu.ro/
+ * @copyright Copyright (c) 2012 Albulescu Cosmin. (http://www.albulescu.com)
+ * @license   http://www.albulescu.ro/new-bsd New BSD License
+ * @autor Albulescu Cosmin <cosmin@albulescu.ro>
+ */
 
 namespace CaSecurity\Rule;
 
--- src/RuleProvider.php
+++ src/RuleProvider.php
@@ -1,13 +1,11 @@
 <?php
 /**
+ * Albulescu Cosmin ( http://www.albulescu.ro/ )
  *
- * Albulescu Cosmin ( http://www.albulescu.com/ )
- *
- * @link      http://www.albulescu.com/
+ * @link      http://www.albulescu.ro/
  * @copyright Copyright (c) 2012 Albulescu Cosmin. (http://www.albulescu.com)
  * @license   http://www.albulescu.ro/new-bsd New BSD License
  * @autor Albulescu Cosmin <cosmin@albulescu.ro>
- *
  */
 
 namespace CaSecurity;
